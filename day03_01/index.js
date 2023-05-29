d385437401b791f91fd49233a375e9046f833f4d
ARahim-S <arahim.s@yandex.com>
2023-05-29 15:51:06 +0300

    updated

--- day03_01/index.js
+++ day03_01/index.js
@@ -78,3 +78,11 @@
 // }
 
 // vowelsAndConsonants("javascriptloops");
+
+const animals = ["cat", "dog", "bird", "horse"];
+
+animals.forEach((value, index, array) => {
+  console.log("value: ", value);
+  console.log("value parametresinin aldığı index :", index);
+  console.log("array:", array);
+});
