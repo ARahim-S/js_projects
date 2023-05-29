e1575be162d1746a45a8f6e08a62d9ce5bbe045a
ARahim-S <arahim.s@yandex.com>
2023-05-29 15:25:36 +0300

    updated

--- day03_01/index.js
+++ day03_01/index.js
@@ -24,13 +24,13 @@
 // }
 // console.log(stringFormat("aliosman"));
 
-let count = 0;
-let increaseDOM = document.getElementById("increase");
-let decreaseDOM = document.getElementById("decrease");
-let counterDOM = document.querySelector(".counter");
+// let count = 0;
+// let increaseDOM = document.getElementById("increase");
+// let decreaseDOM = document.getElementById("decrease");
+// let counterDOM = document.querySelector(".counter");
 
-increaseDOM.addEventListener("click", handleEvent);
-decreaseDOM.addEventListener("click", handleEvent);
+// increaseDOM.addEventListener("click", handleEvent);
+// decreaseDOM.addEventListener("click", handleEvent);
 
 //first way
 // function handleEvent(e) {
@@ -47,9 +47,34 @@ decreaseDOM.addEventListener("click", handleEvent);
 
 //second way
 
-function handleEvent() {
-  this.id === "increase" ? (count += 1) : count > 0 ? (count -= 1) : null;
-  counterDOM.innerHTML = count;
-}
+// function handleEvent() {
+//   this.id === "increase" ? (count += 1) : count > 0 ? (count -= 1) : null;
+//   counterDOM.innerHTML = count;
+// }
 
-counterDOM.innerHTML = count;
+// counterDOM.innerHTML = count;
+
+// function vowelsAndConsonants(s) {
+//   //javascriptloops
+//   let vowelArr = [];
+//   let notVowelArr = [];
+//   for (let i = 0; i < s.length; i++) {
+//     if (
+//       s[i] == "a" ||
+//       s[i] == "e" ||
+//       s[i] == "i" ||
+//       s[i] == "o" ||
+//       s[i] == "u"
+//     ) {
+//       vowelArr.push(s[i]);
+//     } else {
+//       notVowelArr.push(s[i]);
+//     }
+//   }
+//   let lastArr = vowelArr.concat(notVowelArr);
+//   for (let i = 0; i < lastArr.length; i++) {
+//     console.log(lastArr[i]);
+//   }
+// }
+
+// vowelsAndConsonants("javascriptloops");
